f06e8aaab92733ad9ae9acdd2f78d3d554768904
Konstantin Rodnoi <kotrod@gmail.com>
2020-10-03 00:11:29 +0300

    feat: do task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+const checkParam = (array) => array && array.length;
 
-exports.min = function min (array) {
-  return 0;
+exports.min = (array) => {
+    if (checkParam(array)) {
+        return Math.min(...array);
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = (array) => {
+    if (checkParam(array)) {
+        return Math.max(...array);
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = (array) => {
+    if (checkParam(array)) {
+        return array.reduce( (acc, currVal) => acc + currVal) / array.length;
+    }
+    return 0;
 }
